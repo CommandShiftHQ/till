e4d85807e4a53c3bea2a2c0e20c9f2822e1aa684
Gaz Ellis <gmiellis@yahoo.co.uk>
2018-06-23 02:15:57 +0100

    completed till

--- __tests__/till.test.js
+++ __tests__/till.test.js
@@ -1,10 +1,96 @@
-const {
-  addToBasket,
-  totalPrice,
-  scan,
-  removeFromBasket,
-} = require('../src/till');
+const Till = require('../src/till');
 
+describe('constructor', () => {
+
+  it('returns an object', () => {
+    expect(new Till()).toBeInstanceOf(Object);
+  });
+});
+
+describe('scan', () => {
+
+  it('scans the barcode an returns product name', () => {
+
+    const till = new Till();
+
+    expect(till.scan(123)).toEqual('apple');
+  });
+});
+
+describe('addBasket', () => {
+
+  it('adds a product to the basket', () => {
+
+    const till = new Till();
+    till.addBasket(123);
+
+    expect(till.basket).toEqual([{ name: 'apple', barcode: 123, price: 5 }]);
+
+  });
+  
+  it('adds several products to the basket', () => {
+
+    const till = new Till();
+    till.addBasket(123);
+    till.addBasket(765);
+    till.addBasket(456);
+  
+    expect(till.basket).toEqual([ 
+      { name: 'apple', barcode: 123, price: 5 },
+      { name: 'kiwi', barcode: 765, price: 25 },
+      { name: 'banana', barcode: 456, price: 6 } 
+    ]);
+  
+  });
+
+});
+
+describe('removeItem', () => {
+
+  it('removes an item from the basket by barcode', () => {
+
+    const till = new Till();
+    till.addBasket(123);
+    till.addBasket(765);
+    till.addBasket(456);
+
+    till.removeItem(765);
+
+    expect(till.basket).toEqual([
+      { name: 'apple', barcode: 123, price: 5 },
+      { name: 'banana', barcode: 456, price: 6 } 
+    ]);
+ 
+  });
+
+});
+
+describe('sale', () => {
+
+  it('saves the basket to transaction array, totals the sale an clears the basket', () => {
+
+    const till = new Till();
+    till.addBasket(123);
+    till.addBasket(765);
+    till.addBasket(456);
+    till.sale();
+
+    expect(till.transaction).toEqual([ 
+      { name: 'apple', barcode: 123, price: 5 },
+      { name: 'kiwi', barcode: 765, price: 25 },
+      { name: 'banana', barcode: 456, price: 6 },
+      { Total: 36} 
+    ]);
+
+    expect(till.tTotal).toEqual(36);
+    expect(till.basket).toEqual([]);
+  });
+});
+
+
+
+
+/*
 const apple = {
   barcode: 123,
   price: 5,
@@ -63,3 +149,4 @@ test('removeFromBasket removes the item with the given barcode from the basket',
 
   expect(basket).not.toContain(orange);
 });
+*/
--- src/till.js
+++ src/till.js
@@ -1,28 +1,99 @@
-const scan = (barcode, items) => {
+function Till() {
+
+  this.products = [
+    {
+    name: 'apple',
+    barcode: 123,
+    price: 5,
+    },{
+    name: 'banana',
+    barcode: 456,
+    price: 6
+    },{
+    name: 'orange',
+    barcode: 789,
+    price: 7,
+    },{
+    name: 'pineapple',
+    barcode: 5367,
+    price: 80,
+    },{
+    name: 'kiwi',
+    barcode: 765,
+    price: 25,
+    }
+  ];
+
+  this.basket = [];
+
+  this.transaction = [];
+
+  this.tTotal;
+
+};
+
+Till.prototype.scan = function(barcode) {
+
+  return this.products.find((bar) => bar.barcode === barcode).name;
+
+};
+
+Till.prototype.addBasket = function(barcode) {
+
+  let item = this.products.find((bar) => bar.barcode === barcode);
+  this.basket.push(item);
+
+};
+
+Till.prototype.removeItem = function(barcode) {
+
+  let rmItem = this.basket.find((bar) => bar.barcode === barcode);
+  let idxItem = this.basket.indexOf(rmItem);
+  this.basket.splice(idxItem, 1);
+
+};
+
+Till.prototype.sale = function() {
+
+  this.transaction = this.basket.slice();
+  this.tTotal = this.transaction.reduce(function(x, y) {
+    return x + y.price;
+    },0); 
+  this.transaction.push({'Total': this.tTotal});
+  this.basket = [];
+    
+};
+
+
+
+/*  name: scan ,
+, items) => {
   return items.find((item) => {
     return item.barcode === barcode;
   });
 }
-
-const addToBasket = (item, basket) => {
+  name: addTo,
+  item, basket) => {
   basket.push(item);
 }
-
-const totalPrice = (basket) => {
+  name: total,
+  asket) => {
   return basket.reduce((total, item) => {
     return total + item.price;
   }, 0);
 }
-
-const removeFromBasket = (barcode, basket) => {
-  const item = scan(barcode, basket);
-  const index = basket.indexOf(item);
+  name: remov,
+  t = (barcode, basket) => {
+  name: item ,
+  code, basket);
+  name: index,
+  indexOf(item);
   basket.splice(index, 1);
 }
+*/
+module.exports = Till;
+//  scan,
+//  addToBasket,
+//  totalPrice,
+//  removeFromBasket,
 
-module.exports = {
-  scan,
-  addToBasket,
-  totalPrice,
-  removeFromBasket,
-};

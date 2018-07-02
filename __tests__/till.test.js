7743f7abff9918605360a5191e2a6b25aced7dbf
Gaz Ellis <gmiellis@yahoo.co.uk>
2018-07-02 23:53:28 +0100

    some refactoring and formatting of code

--- __tests__/till.test.js
+++ __tests__/till.test.js
@@ -27,20 +27,20 @@ describe('addBasket', () => {
     expect(till.basket).toEqual([{ name: 'apple', barcode: 123, price: 5 }]);
 
   });
-  
+
   it('adds several products to the basket', () => {
 
     const till = new Till();
     till.addBasket(123);
     till.addBasket(765);
     till.addBasket(456);
-  
-    expect(till.basket).toEqual([ 
+
+    expect(till.basket).toEqual([
       { name: 'apple', barcode: 123, price: 5 },
       { name: 'kiwi', barcode: 765, price: 25 },
-      { name: 'banana', barcode: 456, price: 6 } 
+      { name: 'banana', barcode: 456, price: 6 },
     ]);
-  
+
   });
 
 });
@@ -58,9 +58,9 @@ describe('removeItem', () => {
 
     expect(till.basket).toEqual([
       { name: 'apple', barcode: 123, price: 5 },
-      { name: 'banana', barcode: 456, price: 6 } 
+      { name: 'banana', barcode: 456, price: 6 },
     ]);
- 
+
   });
 
 });
@@ -75,78 +75,14 @@ describe('sale', () => {
     till.addBasket(456);
     till.sale();
 
-    expect(till.transaction).toEqual([ 
+    expect(till.transaction).toEqual([
       { name: 'apple', barcode: 123, price: 5 },
       { name: 'kiwi', barcode: 765, price: 25 },
       { name: 'banana', barcode: 456, price: 6 },
-      { Total: 36} 
+      { Total: 36 },
     ]);
 
     expect(till.tTotal).toEqual(36);
     expect(till.basket).toEqual([]);
   });
 });
-
-
-
-
-/*
-const apple = {
-  barcode: 123,
-  price: 5,
-};
-
-const banana = {
-  barcode: 456,
-  price: 6
-};
-
-const orange = {
-  barcode: 789,
-  price: 7,
-};
-
-const pineapple = {
-  barcode: 5367,
-  price: 80,
-};
-
-const kiwi = {
-  barcode: 765,
-  price: 25,
-};
-
-const items = [
-  apple,
-  banana,
-  orange,
-  pineapple,
-  kiwi, 
-];
-
-test('scan finds an item by its barcode', () => {
-  expect(scan(456, items)).toEqual(banana);
-});
-
-test('addToBasket adds an item to the basket', () => {
-  const basket = [];
-
-  addToBasket(kiwi, basket);
-
-  expect(basket).toContain(kiwi);
-});
-
-test('totalPrice gets the total price of items in the basket', () => {
-  const basket = [orange, orange, pineapple];
-
-  expect(totalPrice(basket)).toEqual(94);
-});
-
-test('removeFromBasket removes the item with the given barcode from the basket', () => {
-  const basket = [kiwi, orange, pineapple];
-
-  removeFromBasket(789, basket);
-
-  expect(basket).not.toContain(orange);
-});
-*/
--- src/till.js
+++ src/till.js
@@ -2,98 +2,66 @@ function Till() {
 
   this.products = [
     {
-    name: 'apple',
-    barcode: 123,
-    price: 5,
-    },{
-    name: 'banana',
-    barcode: 456,
-    price: 6
-    },{
-    name: 'orange',
-    barcode: 789,
-    price: 7,
-    },{
-    name: 'pineapple',
-    barcode: 5367,
-    price: 80,
-    },{
-    name: 'kiwi',
-    barcode: 765,
-    price: 25,
-    }
+      name: 'apple',
+      barcode: 123,
+      price: 5,
+    }, {
+      name: 'banana',
+      barcode: 456,
+      price: 6,
+    }, {
+      name: 'orange',
+      barcode: 789,
+      price: 7,
+    }, {
+      name: 'pineapple',
+      barcode: 5367,
+      price: 80,
+    }, {
+      name: 'kiwi',
+      barcode: 765,
+      price: 25,
+    },
   ];
 
   this.basket = [];
 
   this.transaction = [];
 
-  this.tTotal;
+  this.tTotal = null;
 
 };
 
-Till.prototype.scan = function(barcode) {
+Till.prototype.scan = function (barcode) {
 
   return this.products.find((bar) => bar.barcode === barcode).name;
 
 };
 
-Till.prototype.addBasket = function(barcode) {
+Till.prototype.addBasket = function (barcode) {
 
-  let item = this.products.find((bar) => bar.barcode === barcode);
+  const item = this.products.find((bar) => bar.barcode === barcode);
   this.basket.push(item);
 
 };
 
-Till.prototype.removeItem = function(barcode) {
+Till.prototype.removeItem = function (barcode) {
 
-  let rmItem = this.basket.find((bar) => bar.barcode === barcode);
-  let idxItem = this.basket.indexOf(rmItem);
+  const rmItem = this.basket.find((bar) => bar.barcode === barcode);
+  const idxItem = this.basket.indexOf(rmItem);
   this.basket.splice(idxItem, 1);
 
 };
 
-Till.prototype.sale = function() {
+Till.prototype.sale = function () {
 
   this.transaction = this.basket.slice();
-  this.tTotal = this.transaction.reduce(function(x, y) {
+  this.tTotal = this.transaction.reduce(function (x, y) {
     return x + y.price;
-    },0); 
-  this.transaction.push({'Total': this.tTotal});
+  }, 0);
+  this.transaction.push({ Total: this.tTotal });
   this.basket = [];
-    
+
 };
 
-
-
-/*  name: scan ,
-, items) => {
-  return items.find((item) => {
-    return item.barcode === barcode;
-  });
-}
-  name: addTo,
-  item, basket) => {
-  basket.push(item);
-}
-  name: total,
-  asket) => {
-  return basket.reduce((total, item) => {
-    return total + item.price;
-  }, 0);
-}
-  name: remov,
-  t = (barcode, basket) => {
-  name: item ,
-  code, basket);
-  name: index,
-  indexOf(item);
-  basket.splice(index, 1);
-}
-*/
 module.exports = Till;
-//  scan,
-//  addToBasket,
-//  totalPrice,
-//  removeFromBasket,
-
